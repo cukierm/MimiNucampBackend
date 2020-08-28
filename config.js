ef776767101e0577a36bf864e47efbc23ee4e4cd
Mimi Cukier <cukierm@gmail.com>
2020-08-28 17:59:09 -0400

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook' : {
+        clientId: '312370819978190',
+        clientSecret: '2407908ba02f47bc4ddddfe886c8396d'
+    }
 }
